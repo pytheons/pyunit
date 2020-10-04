ed2128afc028358e4ec2e78897559f8e9ae56c6a
Michael Ligus <maveilthain@gmail.com>
2020-10-04 17:48:05 +0200

    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='pytheons.pyunit',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/pytheons/pyunit',
     license='MIT',
@@ -14,6 +17,8 @@ setup(
         'pyunit is wrapper for PyUnit (uniittest) framework',
         'with one important extension (syntax suger) by `@ test` decorator'
     ]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'docopt'
     ]
